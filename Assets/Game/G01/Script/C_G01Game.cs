5b167fac5ebaa3c0bbf2bdf89ea221153ad35860
xiahailong9000 <875876101@qq.com>
2019-07-21 13:30:13 +0800

    初始化提交

---
+++ Assets/Game/G01/Script/C_G01Game.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace G01 {
+    public class C_G01Game  {
+
+       
+        public static void S_Start() {
+
+        }
+    }
+}
---
+++ Assets/Game/StartScene/StartGame.cs
@@ -0,0 +1,17 @@
+﻿using G01;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    public int gameIndex = 1;
+    void Start() {
+        //GameObject.DontDestroyOnLoad(gameObject);
+        switch (gameIndex) {
+            case 1:
+                C_G01Game.S_Start();
+                break;
+            case 16:
+          
+                break;
+        }
+    }
+}
